d6e14376962e5657a41dc174da9106b20ee8ee8a
Obtavio <octavio.ganchozopaladines@dawsoncollege.qc.ca>
2017-08-30 11:44:43 -0400

    Create and test Address

---
+++ common/test/lib/AdressTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package lib;
+
+/**
+ * @author 1539613
+ *
+ */
+public class AdressTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Address myAddress = new Address("3020", "Sherbrooke");
+		System.out.println(myAddress.getAddress());
+
+	}
+
+}
